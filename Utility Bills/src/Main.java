4688be8740462423aa369c28bf1811a9dbc69197
Luca Alfino <alfinoluca21@gmail.com>
2023-09-09 16:29:46 +0300

    Implemented menu and the Residental rates option

--- Utility Bills/src/Main.java
+++ Utility Bills/src/Main.java
@@ -1,13 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
-
-
+        Scanner userInput = new Scanner(System.in);
         ResidentalRate resident = new ResidentalRate();
-        resident.setElectricalConsumptionAmount(2500);
-        System.out.println(resident.calculateElectricityBill());
-        resident.setCubicMeters(34);
-        System.out.println(resident.calculateWaterBill());
+
+        System.out.println("\nChoose Consumer Type:\nResident - 1\nNon-Resident - 2\nDomestic - 3");
+
+        int choice = userInput.nextInt();
+        switch (choice){
+            case 1:
+                System.out.println("Input electrical consumption:");
+                int residentElectricalconsumption = userInput.nextInt();
+                resident.setElectricalConsumptionAmount(residentElectricalconsumption);
+
+                System.out.println("Input water consumption(in m3)");
+                int residentWaterConsumption = userInput.nextInt();
+                resident.setCubicMeters(residentWaterConsumption);
+
+                System.out.println("Your Payment for electricity: €"+resident.calculateElectricityBill() +" kWh");
+                System.out.println("at a rate of " +resident.getElectricalTariff() +" c/kWh");
+
+                System.out.println("\nYour payment for water: €"+resident.calculateWaterBill());
+                System.out.println("at a rate of €"+resident.getWaterTariff() +" m^3");
+                break;
+
+            case 2:
+                System.out.println("Non-Residental");
+                break;
+
+            case 3:
+                System.out.println("Domestitc");
+
+        }
     }
 }
 
