314498bc1affa08ba14a14d432136cb8c71a6d85
Luca Alfino <alfinoluca21@gmail.com>
2023-09-09 15:32:27 +0300

    succesfully implemented electrical calculator method into resident rate

--- Utility Bills/src/ResidentalRate.java
+++ Utility Bills/src/ResidentalRate.java
@@ -2,30 +2,35 @@ import java.util.Scanner;
 
 public class ResidentalRate extends RateCategory implements CanElectricalCalculator{
 
-private double total = getConsumptionAmount() * getTariff();
+
     @Override
     public double calculateElectricity() {
 
-        if(getConsumptionAmount() <= 0 && getConsumptionAmount() >= 2000)
+        double tariff;
+
+
+        if(getConsumptionAmount() >= 0 && getConsumptionAmount() <= 2000)
         {
-            setTariff(0.1047);
+            tariff = 0.1047;
         }
-        else if(getConsumptionAmount() <= 2001 && getConsumptionAmount() >= 6000)
+        else if(getConsumptionAmount() >= 2001 && getConsumptionAmount() <= 6000)
         {
-            setTariff(0.1298);
+            tariff = 0.1298;
         }
-        else if(getConsumptionAmount() <= 6001 && getConsumptionAmount() >= 10_000)
+        else if(getConsumptionAmount() >= 6001 && getConsumptionAmount() <= 10_000)
         {
-            setTariff(0.1607);
+            tariff = 0.1607;
         }
-        else if(getConsumptionAmount() <= 10_001 && getConsumptionAmount() >= 20_000)
+        else if(getConsumptionAmount() >= 10_001 && getConsumptionAmount() <= 20_000)
         {
-            setTariff(0.3420);
+            tariff = 0.3420;
         }
         else
         {
-            setTariff(0.6076);
+            tariff = 0.6076;
         }
+        setTariff(tariff);
+        double total = getConsumptionAmount() * tariff;
         return total;
     }
 }
