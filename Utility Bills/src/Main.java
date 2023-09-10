b7097c22b151bf57e5ee2eb4527a689caffa2ffe
Luca Alfino <alfinoluca21@gmail.com>
2023-09-10 16:10:21 +0300

    Added error handling

--- Utility Bills/src/Main.java
+++ Utility Bills/src/Main.java
@@ -11,58 +11,99 @@ public class Main {
         int choice = userInput.nextInt();
         switch (choice){
             case 1:
-                var resident = new ResidentalRate();
-                System.out.println("Input electrical consumption(in kWh):");
-                double residentElectricalconsumption = userInput.nextDouble();
-                resident.setElectricalConsumptionAmount(residentElectricalconsumption);
+                try {
+                    var resident = new ResidentalRate();
+                    System.out.println("Input electrical consumption(in kWh):");
+                    double residentElectricalconsumption = userInput.nextDouble();
+                    resident.setElectricalConsumptionAmount(residentElectricalconsumption);
 
-                System.out.println("Input water consumption(in m^3)");
-                double residentWaterConsumption = userInput.nextDouble();
-                resident.setCubicMeters(residentWaterConsumption);
+                    System.out.println("Input water consumption(in m^3)");
+                    double residentWaterConsumption = userInput.nextDouble();
+                    resident.setCubicMeters(residentWaterConsumption);
 
-                double residentElectricFormat = Double.parseDouble(format.format(resident.calculateElectricityBill()));
-                System.out.println("\nYour Payment for electricity: €"+residentElectricFormat);
-                double residentWaterFormat = Double.parseDouble(format.format(resident.calculateWaterBill()));
-                System.out.println("Your payment for water: €"+residentWaterFormat);
-                double residentTotalFormat = Double.parseDouble(format.format(resident.totalAmount()));
-                System.out.println("Total: €" +residentTotalFormat);
+                    if(residentElectricalconsumption < 0 || residentWaterConsumption <0){
+                        throw new IllegalArgumentException("Neither of the values can be negative");
+                    }
+
+                    if(residentElectricalconsumption > Double.MAX_VALUE || residentWaterConsumption < Double.MAX_VALUE){
+                        throw new IllegalArgumentException("Invalid number input");
+                    }
+
+                    double residentElectricFormat = Double.parseDouble(format.format(resident.calculateElectricityBill()));
+                    System.out.println("\nYour Payment for electricity: €"+residentElectricFormat);
+                    double residentWaterFormat = Double.parseDouble(format.format(resident.calculateWaterBill()));
+                    System.out.println("Your payment for water: €"+residentWaterFormat);
+                    double residentTotalFormat = Double.parseDouble(format.format(resident.totalAmount()));
+                    System.out.println("Total: €" +residentTotalFormat);
+                }
+                catch (IllegalArgumentException e){
+                    System.out.println("Error: " +e.getMessage());
+                }
                 break;
 
             case 2:
-                var nonRseidentalRate = new NonRseidentalRate();
-                System.out.println("Input electrical consumption(in kWh):");
-                double nonResidentElectricalInput = userInput.nextDouble();
-                nonRseidentalRate.setElectricalConsumptionAmount(nonResidentElectricalInput);
+                try{
+                    var nonRseidentalRate = new NonRseidentalRate();
+                    System.out.println("Input electrical consumption(in kWh):");
+                    double nonResidentElectricalInput = userInput.nextDouble();
+                    nonRseidentalRate.setElectricalConsumptionAmount(nonResidentElectricalInput);
 
-                System.out.println("Input water consumption(in m^3)");
-                double nonResidentWaterConsumption = userInput.nextDouble();
-                nonRseidentalRate.setCubicMeters(nonResidentWaterConsumption);
+                    System.out.println("Input water consumption(in m^3)");
+                    double nonResidentWaterConsumption = userInput.nextDouble();
+                    nonRseidentalRate.setCubicMeters(nonResidentWaterConsumption);
 
-                double nonResidentElectricFormat = Double.parseDouble(format.format(nonRseidentalRate.calculateElectricityBill()));
-                System.out.println("\nYour Payment for electricity: €"+nonResidentElectricFormat);
-                double nonResidentWaterFormat = Double.parseDouble(format.format(nonRseidentalRate.calculateWaterBill()));
-                System.out.println("Your payment for water: €"+nonResidentWaterFormat);
-                double nonResidentTotalFormat = Double.parseDouble(format.format(nonRseidentalRate.totalAmount()));
-                System.out.println("Total: €" +nonResidentTotalFormat);
+                    if(nonResidentElectricalInput < 0 || nonResidentWaterConsumption <0){
+                        throw new IllegalArgumentException("Neither of the values can be negative");
+                    }
+
+                    if(nonResidentElectricalInput > Double.MAX_VALUE || nonResidentWaterConsumption < Double.MAX_VALUE){
+                        throw new IllegalArgumentException("Invalid number input");
+                    }
+
+                    double nonResidentElectricFormat = Double.parseDouble(format.format(nonRseidentalRate.calculateElectricityBill()));
+                    System.out.println("\nYour Payment for electricity: €"+nonResidentElectricFormat);
+                    double nonResidentWaterFormat = Double.parseDouble(format.format(nonRseidentalRate.calculateWaterBill()));
+                    System.out.println("Your payment for water: €"+nonResidentWaterFormat);
+                    double nonResidentTotalFormat = Double.parseDouble(format.format(nonRseidentalRate.totalAmount()));
+                    System.out.println("Total: €" +nonResidentTotalFormat);
+                }
+                catch (IllegalArgumentException e){
+                    System.out.println("Error: " +e.getMessage());
+                }
                 break;
 
             case 3:
-                var domesticRate = new DomesticRate();
-                System.out.println("Input electrical consumption(in kWh):");
-                double domesticElectricalInput = userInput.nextDouble();
-                domesticRate.setElectricalConsumptionAmount(domesticElectricalInput);
+                try{
+                    var domesticRate = new DomesticRate();
+                    System.out.println("Input electrical consumption(in kWh):");
+                    double domesticElectricalInput = userInput.nextDouble();
+                    domesticRate.setElectricalConsumptionAmount(domesticElectricalInput);
 
-                System.out.println("Input water consumption(in m^3)");
-                double domesticWaterConsumption = userInput.nextDouble();
-                domesticRate.setCubicMeters(domesticWaterConsumption);
+                    System.out.println("Input water consumption(in m^3)");
+                    double domesticWaterConsumption = userInput.nextDouble();
+                    domesticRate.setCubicMeters(domesticWaterConsumption);
 
-                double domesticElectricFormat = Double.parseDouble(format.format(domesticRate.calculateElectricityBill()));
-                System.out.println("\nYour Payment for electricity: €"+domesticElectricFormat);
-                double domesticWaterFormat = Double.parseDouble(format.format(domesticRate.calculateWaterBill()));
-                System.out.println("Your payment for water: €"+domesticWaterFormat);
-                double domesticTotalFormat = Double.parseDouble(format.format(domesticRate.totalAmount()));
-                System.out.println("Total: €" +domesticTotalFormat);
+                    if(domesticElectricalInput < 0 || domesticWaterConsumption <0){
+                        throw new IllegalArgumentException("Neither of the values can be negative");
+                    }
+
+                    if(domesticElectricalInput > Double.MAX_VALUE || domesticWaterConsumption < Double.MAX_VALUE){
+                        throw new IllegalArgumentException("Invalid number input");
+                    }
+
+                    double domesticElectricFormat = Double.parseDouble(format.format(domesticRate.calculateElectricityBill()));
+                    System.out.println("\nYour Payment for electricity: €"+domesticElectricFormat);
+                    double domesticWaterFormat = Double.parseDouble(format.format(domesticRate.calculateWaterBill()));
+                    System.out.println("Your payment for water: €"+domesticWaterFormat);
+                    double domesticTotalFormat = Double.parseDouble(format.format(domesticRate.totalAmount()));
+                    System.out.println("Total: €" +domesticTotalFormat);
+                }
+                catch (IllegalArgumentException e)
+                {
+                    System.out.println("Error: " +e.getMessage());
+                }
                 break;
         }
+        userInput.close();
     }
 }
