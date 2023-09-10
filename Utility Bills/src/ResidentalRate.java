1a5a7989642e931af4535a57c7d685873c62baeb
Luca Alfino <alfinoluca21@gmail.com>
2023-09-10 12:28:20 +0300

    Fixed Water Bill method and fixed Electrical bill for Residental Rate

--- Utility Bills/src/ResidentalRate.java
+++ Utility Bills/src/ResidentalRate.java
@@ -5,35 +5,61 @@ public class ResidentalRate extends RateCategory implements CanUtilityCalculator
     public double calculateElectricityBill() {
 
         double tariff = 0;
+        double tax = 0;
 
 
-        if (getElectricalConsumptionAmount() >= 0 && getElectricalConsumptionAmount() <= 2000) {
+        if (getElectricalConsumptionAmount() <= 2000)
+        {
             tariff = 0.1047;
-        } else if (getElectricalConsumptionAmount() >= 2001 && getElectricalConsumptionAmount() <= 6000) {
-            tariff = 0.1298;
-        } else if (getElectricalConsumptionAmount() >= 6001 && getElectricalConsumptionAmount() <= 10_000) {
-            tariff = 0.1607;
-        } else if (getElectricalConsumptionAmount() >= 10_001 && getElectricalConsumptionAmount() <= 20_000) {
-            tariff = 0.3420;
-        } else {
-            tariff = 0.6076;
+            tax = getElectricalConsumptionAmount() * tariff;
         }
-        setElectricalTariff(tariff);
-        double total = getElectricalConsumptionAmount() * tariff;
-        return total;
+        else if (getElectricalConsumptionAmount() <= 6000) {
+            tariff = 0.1298;
+            tax = 2000 * 0.1047;
+            tax += (getElectricalConsumptionAmount() - 2000) * tariff;
+        }
+        else if (getElectricalConsumptionAmount() <= 10_000)
+        {
+            tariff = 0.1607;
+            tax = 2000 * 0.1047;
+            tax += 4000 * 0.1298;
+            tax += (getElectricalConsumptionAmount() - 6000) * tariff;
+        }
+        else if (getElectricalConsumptionAmount() <= 20_000)
+        {
+            tariff = 0.3420;
+            tax = 2000 * 0.1047;
+            tax += 4000 * 0.1298;
+            tax += 4000 * 0.1607;
+            tax += (getElectricalConsumptionAmount() - 10_000) * tariff;
+        }
+        else
+        {
+            tariff = 0.6076;
+            tax = 2000 * 0.1047;
+            tax += 4000 * 0.1298;
+            tax += 4000 * 0.1607;
+            tax += 10_000 * 0.3420;
+            tax += (getElectricalConsumptionAmount() - 20_000) * tariff;
+        }
+        return tax;
     }
 
     @Override
     public double calculateWaterBill() {
         double tariff = 0;
+        double tax = 0;
 
         if (getCubicMeters() <= 33) {
             tariff = 1.3965;
-        } else if (getCubicMeters() > 33) {
-            tariff = 5.1395;
+            tax = getCubicMeters() * tariff;
         }
-        setWaterTariff(tariff);
-        double total = getCubicMeters() * getWaterTariff();
-        return total;
+        else if (getCubicMeters() > 33)
+        {
+            tariff = 5.1395;
+            tax += 33 * 1.3965;
+            tax += (getCubicMeters() - 33) * tariff;
+        }
+        return tax;
     }
 }
