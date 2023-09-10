d5a754c57567cb01478e071c345ae4d6a7a1c9cc
Luca Alfino <alfinoluca21@gmail.com>
2023-09-10 11:47:56 +0300

    Implemented different calculateElectricityBill method on NonResidentalRate

---
+++ Utility Bills/src/NonRseidentalRate.java
@@ -0,0 +1,95 @@
+public class NonRseidentalRate extends RateCategory implements CanElectricalCalculator{
+    @Override
+    public double calculateElectricityBill() {
+        double tariff = 0;
+        double tax = 0;
+
+
+        if (getElectricalConsumptionAmount() <= 2000)
+        {
+            tariff = 0.1215;
+            tax = getElectricalConsumptionAmount() * tariff;
+        }
+        else if (getElectricalConsumptionAmount() >= 6000) {
+            tariff = 0.1275;
+            tax = 2000 * 0.1215;
+            tax += (getElectricalConsumptionAmount() - 2000) * 0.1275;
+        }
+        else if (getElectricalConsumptionAmount() >= 10_000)
+        {
+            tariff = 0.1373;
+            tax = 2000 * 0.1215;
+            tax += 4000 * 0.1275;
+            tax += (getElectricalConsumptionAmount() - 4000) * 0.1373;
+        }
+        else if (getElectricalConsumptionAmount() >= 20_000)
+        {
+            tariff = 0.1485;
+            tax = 2000 * 0.1215;
+            tax += 4000 * 0.1275;
+            tax += 4000 * 0.1373;
+            tax += (getElectricalConsumptionAmount() - 10_000) * 0.1485;
+        }
+        else if (getElectricalConsumptionAmount() >= 60_000)
+        {
+            tariff = 0.1613;
+            tax = 2000 * 0.1215;
+            tax += 4000 * 0.1275;
+            tax += 4000 * 0.1373;
+            tax += 10_000 * 0.1485;
+            tax += (getElectricalConsumptionAmount() - 40_000) * 0.1613;
+        }
+        else if (getElectricalConsumptionAmount() >= 100_000)
+        {
+            tariff = 0.1500;
+            tax = 2000 * 0.1215;
+            tax += 4000 * 0.1275;
+            tax += 4000 * 0.1373;
+            tax += 10_000 * 0.3420;
+            tax += 40_000 * 0.1613;
+            tax += (getElectricalConsumptionAmount() - 40_000) * 0.1500;
+        }
+        else if (getElectricalConsumptionAmount() >= 1_000_000)
+        {
+            tariff = 0.1403;
+            tax = 2000 * 0.1215;
+            tax += 4000 * 0.1275;
+            tax += 4000 * 0.1373;
+            tax += 10_000 * 0.3420;
+            tax += 40_000 * 0.1613;
+            tax += 40_000 * 0.1500;
+            tax += (getElectricalConsumptionAmount() - 900_000) * 0.1403;
+        }
+        else if (getElectricalConsumptionAmount() >= 5_000_000)
+        {
+            tariff = 0.1275;
+            tax = 2000 * 0.1215;
+            tax += 4000 * 0.1275;
+            tax += 4000 * 0.1373;
+            tax += 10_000 * 0.3420;
+            tax += 40_000 * 0.1613;
+            tax += 40_000 * 0.1500;
+            tax += 900_000 * 0.1403;
+            tax += (getElectricalConsumptionAmount() - 4_000_000) * 0.1275;
+        }
+        else
+        {
+            tariff = 0.6076;
+            tax = 2000 * 0.1215;
+            tax += 4000 * 0.1275;
+            tax += 4000 * 0.1373;
+            tax += 10_000 * 0.3420;
+            tax += 40_000 * 0.1613;
+            tax += 40_000 * 0.1500;
+            tax += 900_000 * 0.1403;
+            tax += 4_000_000 * 0.1275;
+            tax += (getElectricalConsumptionAmount() - 5_000_000) * 0.1080;
+        }
+        return tax;
+    }
+
+    @Override
+    public double calculateWaterBill() {
+        return 0;
+    }
+}
